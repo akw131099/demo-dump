68d8f6dac1515cb694eb40017df988f8dd2204a5
Alex Kieran Wright <alex.092@live.com>
2024-12-11 13:34:24 +0000

    added background colour changers for cyan and papyawhite using JS

--- auth.js
+++
@@ -1,1 +0,0 @@
-const verifiedUsers = ["AlexW", "MaxK"];
---
+++ background.js
@@ -0,0 +1,22 @@
+const cyanSelector = document.getElementById("cyan-selector");
+
+cyanSelector.addEventListener("click", handleClickCyan);
+
+function handleClickCyan() {
+  document.body.style.backgroundColor = "cyan";
+}
+
+const papayawhipSelector = document.getElementById("papayawhip-selector");
+
+papayawhipSelector.addEventListener("click", handleClickPapaya);
+
+function handleClickPapaya() {
+  document.body.style.backgroundColor = "papayawhip";
+}
+
+// const buttonColour = document.getElementById("button-colour");
+// buttonColour.addEventListener("click", handleClickColour);
+
+// function handleClickColour() {
+//   document.body.style.backgroundColor = "blue";
+// }
